77df2b871aa8b8df0adb72399242f5474393b9df
Mukeshuday <your-email@used-with-Mukeshuday>
2025-07-05 17:09:53 +0530

    Portfolio successfully pushed

---
+++ PortfolioRise/script.js
@@ -0,0 +1,34 @@
+
+
+function downloadResume() {
+  const link = document.createElement("a");
+  link.href = "./image/Mukeshresume main.pdf";
+  link.download="Mukesh_UdayaKumar_Resume.pdf";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
+
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const name = document.getElementById("name").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const message = document.getElementById("message").value.trim();
+
+  if (name && email && message) {
+    localStorage.setItem("contact", JSON.stringify({ name, email, message }));
+    document.getElementById("success-banner").style.display = "block";
+
+    setTimeout(() => {
+      document.getElementById("success-banner").style.display = "none";
+    }, 3000);
+
+    this.reset();
+  } else {
+    alert("Please fill out all fields.");
+  }
+});
+
+
